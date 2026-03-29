6424e023707fb93f1d24545b32eb099dc12bc256
Siddharth Sarkar <sidsarkar2112@gmail.com>
2026-03-29 09:46:17 +0530

    db connection setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
+import dbConnect from "@/lib/dbConnect";
 import Image from "next/image";
 
 export default function Home() {
-  
-
-
+  dbConnect();
 
   return <>learn TypeScript with chai or code</>;
 }
---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+  isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Already connected to database");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.DB_URL || "");
+    connection.isConnected = db.connections[0].readyState;
+    console.log(db.connections[0], "Database connected successfully");
+    
+  } catch (error) {
+    console.log("Database connection Failed", error);
+    process.exit(1);
+  }
+}
+
+export default dbConnect;
