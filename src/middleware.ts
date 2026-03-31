69d9bd560efd69450c407844e028bbde4992cf3d
Siddharth Sarkar <sidsarkar2112@gmail.com>
2026-03-31 14:55:03 +0530

    setup nextAuth

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+export { default } from "next-auth/middleware";
+import { getToken } from "next-auth/jwt";
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+  const token = await getToken({ req: request });
+  const url = request.nextUrl;
+
+  if (
+    token &&
+    (url.pathname.startsWith("/sign-in") || url.pathname.startsWith("/sign-up"))
+  ) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  // if (!token) {
+  //   return NextResponse.redirect(new URL("/sign-up", request.url));
+  // }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/sign-in", "/sign-up"],
+};
